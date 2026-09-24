414effc65d1de2153679b5761dd0051f8093871e
unknown <wuendolinvalverde@gmail.com>
2026-09-23 21:06:24 -0600

    feat: agregar entidades y configuracion de persistencia

---
+++ src/BattleHub.Memory.Data/Migrations/20260924030200_ConfigurarGameResultPlayers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BattleHub.Memory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigurarGameResultPlayers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
